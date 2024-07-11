34d08fc290b27dd3c9a99c92c3c9da6b68d4d580
Chaitanya Paraskar <cvparaskar2004@gmail.com>
2024-07-11 19:41:58 +0530

    Regular Commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI Powered Prompts</span>
+                <p className="desc text-center">
+                    Promptia is an Open-Source AI Prompting Tool for Modern World to Discover, Create and Share Creative Prompts.
+                </p>
+            </h1>
+        </section>
     )
 }
 export default Home;
