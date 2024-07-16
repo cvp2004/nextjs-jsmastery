9440b1425a33c1c57c7d75e5cc6b5c3ea6f1eef4
Chaitanya Paraskar <cvparaskar2004@gmail.com>
2024-07-16 23:40:16 +0530

    Regular Commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app)
+
+
+## Problems Unsolved
+
+1. creator data not beign sent to DB.
+
 
 ## Course Link
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,6 +1,28 @@
-const PromptCard = () => {
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+const PromptCard = ({ data, handleTagClick }) => {
+
+    console.log(data);
+
     return (
-        <div>PromptCard</div>
+        <div className="prompt_card">
+            <div className="flex justify-between items-start gap-5">
+                <div className="flex-1 justify-start items-center gap-3 cursor-pointer">
+                    <Image
+                        src={data.creator.image}
+                        alt="user_image"
+                        width={40}
+                        height={40}
+                        className="rounded-full object-contain"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 export default PromptCard
